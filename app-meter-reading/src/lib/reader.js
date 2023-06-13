9c3b06067daaaddfaa9c45d81d9ebda18b30ee28
Mark Harrison <yourpropertyexpert@gmail.com>
2023-06-13 20:57:29 +0100

    Axios version

--- app-meter-reading/src/lib/reader.js
+++ app-meter-reading/src/lib/reader.js
@@ -1,5 +1,13 @@
 function getTokenFromUNPW(username, password) {
-    return "DummyToken"
+
+    const PostData = new FormData()
+    PostData.append('username', username)
+    PostData.append('password', password)
+
+    axios.post('https://api.glowmarkt.com/v0-1/auth', PostData)
+      .then(response => {
+        return (response.data)
+      })
 }
 
 export default {
