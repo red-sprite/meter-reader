c427b4c16a1e8e460dfbba228465b97087fb69e8
Kevin Golding <kevin@kgolding.co.uk>
2023-06-13 20:41:28 +0100

    added svelte app

---
+++ app-meter-reading/src/App.svelte
@@ -0,0 +1,14 @@
+
+<main>
+  <h1>Red-Sprite Meter Readings</h1>
+
+  <p>Token: { token }</p>
+
+</main>
+
+<script>
+import reader from "./lib/reader.js";
+
+const token = reader.getTokenFromUNPW("", "")
+
+</script>
