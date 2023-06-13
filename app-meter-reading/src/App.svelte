9582bf31810dea57369002ada64f8e5d2a337f4d
Kevin Golding <kevin@kgolding.co.uk>
2023-06-13 21:56:37 +0100

    minimum working web app

--- app-meter-reading/src/App.svelte
+++ app-meter-reading/src/App.svelte
@@ -1,14 +1,64 @@
+<script>
+  import reader from "./lib/reader.js";
+
+  let username = localStorage.getItem("username");
+  let password = localStorage.getItem("password");
+  let entities = [];
+  let display = [];
+
+  function login() {
+    reader.getTokenFromUNPW(username, password).then(() => {
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+      reader.getEntities().then((data) => {
+        entities = data;
+        entities.forEach((e) => {
+          console.log("e", e);
+          e.resources.forEach((r) => {
+            reader.getReadings(r.resourceId).then((data) => {
+              display = [...display, data];
+              console.log("display", display)
+            });
+          });
+        });
+      });
+    });
+  }
+</script>
 
 <main>
   <h1>Red-Sprite Meter Readings</h1>
 
-  <p>Token: { token }</p>
+  <form>
+    Username: <input type="text" bind:value={username} /><br />
+    Password: <input type="password" bind:value={password} /><br />
 
+    <button type="button" on:click={login}>Login</button>
+  </form>
+
+  {#if entities.length > 0}
+    <div>
+      {#each entities as e}
+        {#each e.resources as r}
+          <div>{r.name}</div>
+        {/each}
+      {/each}
+    </div>
+  {/if}
+  {#if display.length > 0}
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    {#each display as d}
+    <tr>
+      <td>{ d.name}</td>
+      {#each d.data as a}
+        <td>{new Date(a[0]*1000).toLocaleString()} -- {a[1]}</td>
+      {/each}
+    </tr>
+    {/each}
+  </table>
+    
+  {/if}
 </main>
-
-<script>
-import reader from "./lib/reader.js";
-
-const token = reader.getTokenFromUNPW("", "")
-
-</script>
--- app-meter-reading/src/lib/reader.js
+++ app-meter-reading/src/lib/reader.js
@@ -1,15 +1,65 @@
+var token;
+
 function getTokenFromUNPW(username, password) {
+    return new Promise((resolve, reject) => {
+        fetch('https://api.glowmarkt.com/api/v0-1/auth', {
+            method: 'post',
+            headers: {
+                "Content-Type": "application/json",
+                "applicationId": "b0f1b774-a586-4f72-9edd-27ead8aa7a8d"
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        }).then(resp => {
+            resp.json().then(data => {
+                token = data.token
+                console.log(token)
+                resolve(token)
+            })
+        })
+    })
+}
 
-    const PostData = new FormData()
-    PostData.append('username', username)
-    PostData.append('password', password)
+function getEntities() {
+    return new Promise((resolve, reject) => {
+        fetch('https://api.glowmarkt.com/api/v0-1/virtualentity', {
+            method: 'get',
+            headers: {
+                "Content-Type": "application/json",
+                "applicationId": "b0f1b774-a586-4f72-9edd-27ead8aa7a8d",
+                token
+            },
+        }).then(resp => {
+            resp.json().then(data => {
+                console.log(data)
+                resolve(data)
+            })
+        })
+    })
+}
 
-    axios.post('https://api.glowmarkt.com/v0-1/auth', PostData)
-      .then(response => {
-        return (response.data)
-      })
+function getReadings(resourceId) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://api.glowmarkt.com/api/v0-1/resource/${resourceId}/readings?from=2023-01-01T00:00:00&to=2023-01-02T00:00:00&period=PT1H&offset=0&function=sum`, {
+            method: 'get',
+            headers: {
+                "Content-Type": "application/json",
+                "applicationId": "b0f1b774-a586-4f72-9edd-27ead8aa7a8d",
+                token
+            },
+        }).then(resp => {
+            resp.json().then(data => {
+                console.log("readings", data)
+                resolve(data)
+            })
+        })
+    })
 }
 
 export default {
-    getTokenFromUNPW
+    getTokenFromUNPW,
+    getEntities,
+    getReadings
 }
